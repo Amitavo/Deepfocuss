bb140da6fb083946e3508ee3a3534042749b4949
Amitavo <44457119+Amitavo@users.noreply.github.com>
2020-03-05 17:20:08 +0530

    unwanted files deleted

---
+++ resources/js/script.js
@@ -0,0 +1,13 @@
+/*------- MAKES H1 BG RED -------*/
+
+$(document).ready(function() {
+
+
+
+    $('h1').on("click", function() {
+
+        $(this).css('background-color', '#ff0000');
+
+    });
+
+});
